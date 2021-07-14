abfcc1bc9e4ebdb6072f1ba8be6c621f102b77ba
berezka-creator <berezina.y.v.2015@gmail.com>
2021-07-14 00:29:46 -0700

    fixed form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,32 +1,118 @@
-import { useState } from 'react'
-import "./Contact.css"
-
-export default function Contact() {
-
-    const [message,setMessage] = useState (false)
+import { Component } from 'react'
+import emailjs from 'emailjs-com'
+import{ init } from 'emailjs-com';
 
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true)
+import "./Contact.css";
+import Input from '../UI/Input.jsx';
 
+class Contact extends Component {
+
+    state = {
+        orderForm:{
+            name:{
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Your name"
+                },
+                value: ''
+            },
+            email:{
+                elementType: 'input',
+                elementConfig: {
+                    type: "email",
+                    placeholder: "Your email..."
+                },
+                value: ''
+            },
+            message: {
+                elementType: 'textarea',
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Your message..."
+                },
+                value: ''
+
+            }
+
+
+        },
+        loading: false,
+        message: false
     }
-    return (
-        <div className="contact" id="contact">
-            <div className="left">
-                <img src="assets/shake.svg" alt="" />
+
+    inputChangedHandler = (event, inputIdentifier) => {
+   
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+     
+        updatedFormElement.value = event.target.value;
+
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});  
+    }
+    
+
+    sendEmail = (e) => {
+        e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_83aqx4c', e.target, "user_foLN1bYDlF1jKYwQM8UUe")
+    
+    .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+       
+    }
+
+    render () {
+
+        const formElementArray = [];
+        for (let key in this.state.orderForm){
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        let form = (
+            <form onSubmit={this.sendEmail}>
+           
+                 {formElementArray.map(formElement => (
+                     <Input 
+                        key= {formElement.id}
+                        elementType = {formElement.config.elementType}
+                        elementConfig = {formElement.config.elementConfig}
+                        value = {formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                 ))}
+                 <button type="submit">Send</button>
+
+            </form>
+        )
+
+
+        return (
+            <div className="contact" id="contact">
+                <div className="left">
+                    <img src="assets/shake.svg" alt="" />
+                </div>
+                <div className="right">
+                    <h2>Contact</h2>
+                        {form}
+                       
+                </div>
+                
             </div>
-            <div className="right">
-                <h2>Contact</h2>
-                <form onSubmit={handleSubmit} action="mailto:berezina.y.v.2015@gmail.com">
-                    <input type="text" placeholder="Email" />
-                    <textarea placehilder="Message"></textarea>
-                    <button type="submit">Send</button>
-                    {message && <span>Thanks, I will reply ASAP :) </span>}
-               </form>
-            </div>
-            
-        </div>
-    )
+        );
+     } 
 }
+export default Contact;
 
