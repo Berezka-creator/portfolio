d1a478d032e35902067e3075c926a7c962dd9a2d
berezka-creator <berezina.y.v.2015@gmail.com>
2021-08-16 20:08:40 -0700

    changed directory for images in portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,6 +8,11 @@ import  {
 } from "../../data";
 import { HashLink as Link } from 'react-router-hash-link'
 
+import burgerApp from './images/burger-app.png'
+import hostApp from './images/host-app.png'
+import sfLiving from './images/sfliving.png'
+import youtubeQuiz from './images/youtubeQuiz.png'
+
 
 
 export default function Portfolio() {
@@ -23,28 +28,27 @@ export default function Portfolio() {
             <div className="container">
                 <div className="item">
                     <Link to='/projectspage#burger'>
-                        <img src="./assets/burger-app.png" alt="" />
+                        <img src={burgerApp} alt="" />
                     </Link>
                     <h3>Burger Builder</h3>
                 </div>
                    
                     <div className="item">
                         <Link to='/projectspage#host'>
-                            <img src="./assets/host-app.png" alt="" />
+                            <img src={hostApp} alt="" />
                         </Link>
                         <h3>Hosting App</h3>
                     </div>
-
                     <div className="item">
                         <Link to='/projectspage#youtube'>
-                            <img src="./assets/youtubeQuiz.png" alt="" />    
+                            <img src={youtubeQuiz} alt="" />    
                         </Link>
                         <h3>Youtube Quiz</h3>
                     </div>
 
                     <div className="item">
                         <Link to='/projectspage#sf'>
-                            <img src="./assets/sfliving.png" alt="" />
+                            <img src={sfLiving} alt="" />
                         </Link>
                         <h3>WordPress</h3>
                     </div>       
