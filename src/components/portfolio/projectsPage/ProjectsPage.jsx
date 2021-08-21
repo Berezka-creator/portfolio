66aa593472547381c7dede67ff4b19bbb0e6a9f3
berezka-creator <berezina.y.v.2015@gmail.com>
2021-08-21 09:27:36 -0700

    changed url links in projects page and cleaned projectsPage component

--- src/components/portfolio/projectsPage/ProjectsPage.jsx
+++ src/components/portfolio/projectsPage/ProjectsPage.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './ProjectsPage.css'
 import ProjectItem from './project/BurgerProject'
 import arrow from "../../../images/arrow.png"
-
-
 import burgerApp from '../../../images/burger-app.png'
 import hostApp from '../../../images/host-app.png'
 import sfLiving from '../../../images/sfliving.png'
@@ -21,7 +19,6 @@ export default function ProjectsPage() {
             github: "https://github.com/Berezka-creator/burger-app",
             liveview: "https://berezka-creator.github.io/burger-app/",
             tools: [ "HTML", "CSS", "JavaScript","ReactJS", "Redux", "API", "Fairbase"],
-            
             desc: "This is mobile app to create your custom burger, you can choose ingredients and see how your burger will look like and how much will it cost, than you can procced to checkout and see your orders in orders page.  and added it in your orders, ",
             chell: "It was very difficultbu fun"
         },
@@ -33,7 +30,6 @@ export default function ProjectsPage() {
             github: "https://github.com/Berezka-creator/academind_css_project",
             liveview: "https://berezka-creator.github.io/burger-app/",
             tools: [ "HTML", "CSS", "Sass", "JavaScript","ReactJS"],
-            
             desc: "It is an app that provides hosting to the customers, contains four pages with description, testimonials and packages and a form page.",
             chell: "It was very difficult but fun"
         },
@@ -45,7 +41,6 @@ export default function ProjectsPage() {
             github: "https://github.com/Berezka-creator/qtube",
             liveview: "https://berezka-creator.github.io/qtube/",
             tools: [ "HTML", "CSS", "JavaScript", "JQuery"],
-            
             desc: "This is a video app for children that will b allowed to watch video just after answer question right.",
             chell: "It was very difficultbu fun"
         },
@@ -54,14 +49,12 @@ export default function ProjectsPage() {
             idcontainer: "sf",
             title: "SF Living Wage",
             img: sfLiving,
-            github: "https://github.com/Berezka-creator/burger-app",
+            github: "#",
             liveview: "https://www.csej.org/",
             tools: [ "WordPress", "Elementor"],
-            
             desc: "This is a film festival websilte that I made while my volonteering with SF Living Wage organisation, personally I made a main page and about page,also Movie list. ",
             chell: "It was very difficultbu fun"
         }
-
     ]
 
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -76,32 +69,23 @@ export default function ProjectsPage() {
     return (
         <div className="works">
             <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
-            {data.map((d) => (
-               <div className="container" id={d.idcontainer}>
-                   <div className="item">
-                   <ProjectItem 
-                        title={d.title}
-                        img={d.img}
-                        gitHubLink={d.github}
-                        liveLink={d.liveview}
-                        tools={d.tools}
-                        desc={d.desc}>
-                  </ProjectItem>
-
-                   </div>
-               </div>
-              
-                   
-
-                ))
-            }
-        
-        </div>
-                 
-         
-
+                {data.map((d) => (
+                    <div className="container" id={d.idcontainer}>
+                        <div className="item">
+                            <ProjectItem 
+                                title={d.title}
+                                img={d.img}
+                                gitHubLink={d.github}
+                                liveLink={d.liveview}
+                                tools={d.tools}
+                                desc={d.desc}>
+                            </ProjectItem>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            
            <img src={arrow} className="arrow left" alt="" onClick ={() => handleClick("left")}/>
-          
            <img src={arrow} className="arrow right" alt="" onClick ={() => handleClick("rigth")}/>
            
         </div>
