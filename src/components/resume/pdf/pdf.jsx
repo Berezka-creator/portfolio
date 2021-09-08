14f311b127fe32f3a8296c1209a0811de0efd290
berezka-creator <berezina.y.v.2015@gmail.com>
2021-09-08 01:00:23 -0700

    pdf

--- src/components/resume/pdf/pdf.jsx
+++ src/components/resume/pdf/pdf.jsx
@@ -5,6 +5,7 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 import { Worker } from '@react-pdf-viewer/core'
+import PDF from './Julia_Berezina_resume.pdf'
 
 
 export const pdf = () => {
@@ -14,7 +15,7 @@ export const pdf = () => {
     return (
         <div className="pdf-container">
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-                 <Viewer fileUrl="./Julia_Berezina_Resume.pdf" 
+                 <Viewer fileUrl={PDF} 
                  plugins={[
                     defaultLayoutPluginInstance
                 ]} />
