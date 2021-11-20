598b7c5220500501560c81aa3eeda25b5a603e1f
berezka-creator <berezina.y.v.2015@gmail.com>
2021-11-20 11:59:40 -0800

    Added aditional portfolio images to project page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,6 +12,7 @@ import burgerApp from '../../images/burger-app.png'
 import hostApp from '../../images/host-app.png'
 import sfLiving from '../../images/sfliving.png'
 import youtubeQuiz from '../../images/youtubeQuiz.png'
+import foodApp from '../../images/food-app.png'
 
 
 
@@ -27,28 +28,35 @@ export default function Portfolio() {
                 <Link to='/projectspage#burger'>
                 <div className="item">
                     <img src={burgerApp} alt="Burger Builder Web Application" />
-                    <h3>Burger Builder</h3>
+                    <h3>React Burger Builder</h3>
+                </div>
+            </Link>
+
+            <Link to='/projectspage#food'>
+                <div className="item">
+                    <img src={foodApp} alt="Restarant Web Application" />
+                    <h3>React Restaurant App</h3>
                 </div>
             </Link>
 
                 <Link to='/projectspage#host'>      
                 <div className="item">
                     <img src={hostApp} alt="Hosting Website" />
-                    <h3>Hosting Website</h3>
+                    <h3>HTML/CSS Hosting Website</h3>
                 </div>
             </Link>
 
                 <Link to='/projectspage#youtube'>       
                 <div className="item">
                     <img src={youtubeQuiz} alt="Youtube API Application" />
-                    <h3>Youtube API Application </h3>
+                    <h3>Youtube API Application with Quiz</h3>
                 </div>
             </Link>
 
                 <Link to='/projectspage#sf'>       
                 <div className="item">
                     <img src={sfLiving} alt="Film Festival" />
-                    <h3>Film Festival</h3>
+                    <h3>WordPress Film Festival</h3>
                 </div>   
             </Link>
 
