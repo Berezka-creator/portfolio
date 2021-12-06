eb4ffd54f357b7b2d860a1cc04dc737f36e71839
berezka-creator <berezina.y.v.2015@gmail.com>
2021-12-05 21:19:01 -0800

    Removed links from projects todescriptions from portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -44,47 +44,46 @@ export default function Portfolio() {
 
             <div className="container">
 
-                <Link to='/portfolio#burger'>
+           
                 <div className="item" key ="1">
                     <img src={burgerApp} alt="Burger Builder Web Application" />
                     <h3>React Burger Builder</h3>
                 </div>
-            </Link>
-
-                <Link to='/portfolio#host' >      
+      
+    
                 <div className="item"  key ="2">
                     <img src={hostApp} alt="Hosting Website" />
                     <h3>HTML/CSS Hosting Website</h3>
                 </div>
-            </Link>
+       
 
-                <a href='#youtube'>       
+                 
                 <div className="item"  key ="3">
                     <img src={youtubeQuiz} alt="Youtube API Application" />
                     <h3>Youtube API Application with Quiz</h3>
                 </div>
-            </a>
+      
 
-            <Link to='/portfolio#food'>
+         
                 <div className="item"  key ="4">
                     <img src={foodApp} alt="Restarant Web Application" />
                     <h3>React Restaurant App</h3>
                 </div>
-            </Link>
+        
 
-                <Link to='/portfolio#sf'>       
+                 
                 <div className="item"  key ="5">
                     <img src={sfLiving} alt="Film Festival" />
                     <h3>WordPress Film Festival</h3>
                 </div>   
-            </Link>
+        
 
-            <Link to='/portfolio#samara'>       
+            
                 <div className="item"  key ="6">
                     <img src={samara} alt="Samara" />
                     <h3>WordPress Samara Group</h3>
                 </div>   
-            </Link>
+    
             </div>
         </div>
     );
